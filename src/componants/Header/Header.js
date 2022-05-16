d12b26a5340c9db4833740210b605ee598c9d294
Asaduzzaman Alamin <asaduzzamanrossow2000@gmail.com>
2022-05-16 12:00:52 -0700

    header-start-part

--- src/componants/Header/Header.js
+++ src/componants/Header/Header.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../image/images-removebg-preview.png'
 
 const Header = () => {
     return (
         <div>
             <h2>this is header</h2>
+        <div className='header-container'>
+            <div className='logo-container'>
+                <img src={Logo} alt="" />
+
+            </div>
+            <div className='menu-container'>
+                <Link to='/'>Home</Link>
+                <Link to='/'>Cricket Team</Link>
+                <Link to='/'>Legend Player</Link>
+                <Link to='/'>ABout </Link>
+                <Link to='/'>contact</Link>
+
+            </div>
+
+        </div>
         </div>
     );
 };
