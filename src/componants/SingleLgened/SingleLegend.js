03c74c3f0f17af76b0fd285be90876be81add35b
Asaduzzaman Alamin <asaduzzamanrossow2000@gmail.com>
2022-05-20 05:11:13 -0700

    holo

--- src/componants/SingleLgened/SingleLegend.js
+++ src/componants/SingleLgened/SingleLegend.js
@@ -9,7 +9,7 @@ const SingleLegend = (props) => {
         <div className='single-lgd'>
            <img className='lgd-pic' src={picturelegend} alt="" />
            <h4 className='name'>{namelegend}</h4>
-           <h4 className='name'>Age: {agelegend}</h4>
+           <h4 className='name'>Age: {agelegend}</h4>   
            <p>{aboutlegend}</p>
         </div>
     );
